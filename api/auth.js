37718b5b4c63bf2baba34c82bd22cebf445c35b5
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 10:54:20 -0500

    fix: add deno version

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env deno run --version v2.1.5
 import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
 
 module.exports = createVercelBeginHandler({}, { useEnv: true })
--- api/callback.js
+++ api/callback.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env deno run --version v2.1.5
 import { createVercelCompleteHandler } from "netlify-cms-oauth-provider-node"
 
 module.exports = createVercelCompleteHandler({}, { useEnv: true })

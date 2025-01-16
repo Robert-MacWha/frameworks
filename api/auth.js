8335098b98ee4c836a5f811b6fefb60d37327115
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 10:59:42 -0500

    feat: downgrade deno

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env deno run --version v2.1.5
 import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
 
 module.exports = createVercelBeginHandler({}, { useEnv: true })
--- api/callback.js
+++ api/callback.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env deno run --version v2.1.5
 import { createVercelCompleteHandler } from "netlify-cms-oauth-provider-node"
 
 module.exports = createVercelCompleteHandler({}, { useEnv: true })

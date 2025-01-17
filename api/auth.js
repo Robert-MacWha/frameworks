f2380f94d428a1d9f7e12d4b4e0d3ff60a8cb327
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 21:57:12 -0500

    feat: revert to custom auth

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,16 @@
-import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
+// import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
+import crypto from 'crypto';
 
-export default createVercelBeginHandler({}, { useEnv: true })
+export default async function handler(req, res) {
+    // Generate a random state for CSRF protection
+    const state = crypto.randomBytes(8).toString('hex');
+    const scope = 'public_repo'; // Scopes required for the app
+
+    // Build the authorization URI
+    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.CALLBACK_REDIRECT_URI}&scope=${encodeURIComponent(scope)}&response_type=code&state=${state}`;
+
+    // Respond with a redirect to the authorization URI
+    res.status(302); // HTTP 302 Found
+    res.setHeader('Location', authorizationUri);
+    res.end();
+}

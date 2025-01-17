1862d8f5b2fdfa4677145570d156fc2348cf36e7
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 21:58:36 -0500

    feat: fix redirect_uri

--- api/auth.js
+++ api/auth.js
@@ -4,10 +4,11 @@ import crypto from 'crypto';
 export default async function handler(req, res) {
     // Generate a random state for CSRF protection
     const state = crypto.randomBytes(8).toString('hex');
-    const scope = 'repo'; // Scopes required for the app
+    const scope = 'public_repo'; // Scopes required for the app
+    const redirectUri = "https://" + process.env.CALLBACK_REDIRECT_URI + "/api/callback";
 
     // Build the authorization URI
-    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.CALLBACK_REDIRECT_URI}&scope=${encodeURIComponent(scope)}&response_type=code&state=${state}`;
+    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}&response_type=code&state=${state}`;
 
     // Respond with a redirect to the authorization URI
     res.status(302); // HTTP 302 Found

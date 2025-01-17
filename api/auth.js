f523bd9cf9c157b2cf6004c6b80fe7843ea21604
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 21:44:00 -0500

    revert: switch back to `createVercelBeginHandler`

--- api/auth.js
+++ api/auth.js
@@ -1,16 +1,3 @@
 import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
-import crypto from 'crypto';
 
-export default async function handler(req, res) {
-    const state = crypto.randomBytes(8).toString('hex');
-    const scope = 'repo,user';
-    const callback_url = "https://" + process.env.ORIGIN + '/api/callback';
-
-    // Build the authorization URI
-    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&redirect_uri=${callback_url}&scope=${encodeURIComponent(scope)}&response_type=code&state=${state}`;
-
-    // Respond with a redirect to the authorization URI
-    res.status(302);
-    res.setHeader('Location', authorizationUri);
-    res.end();
-}
+export default createVercelBeginHandler({}, { useEnv: true })

f78a51ae53f201f8d2deb426d2b7c175da37173e
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 21:32:37 -0500

    feat: add scoped permission request auth

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,15 @@
 import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
 
-export default createVercelBeginHandler({}, { useEnv: true })
+export default async function handler(req, res) {
+    const state = crypto.randomBytes(8).toString('hex');
+    const scope = 'repo,user';
+    const callback_url = process.env.ORIGIN + '/api/callback';
+
+    // Build the authorization URI
+    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&redirect_uri=${callback_url}&scope=${encodeURIComponent(scope)}&response_type=code&state=${state}`;
+
+    // Respond with a redirect to the authorization URI
+    res.status(302);
+    res.setHeader('Location', authorizationUri);
+    res.end();
+}

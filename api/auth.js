ffc76cd85d0f4f375fce816d5239bc536e2d01ae
Robert MacWha <trebor.ahwcam@gmail.com>
2025-01-16 22:04:55 -0500

    feat: remove scope

--- api/auth.js
+++ api/auth.js
@@ -4,11 +4,10 @@ import crypto from 'crypto';
 export default async function handler(req, res) {
     // Generate a random state for CSRF protection
     const state = crypto.randomBytes(8).toString('hex');
-    const scope = 'public_repo'; // Scopes required for the app
     const redirectUri = "https://" + process.env.ORIGIN + "/api/callback";
 
     // Build the authorization URI
-    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}&response_type=code&state=${state}`;
+    const authorizationUri = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&state=${state}`;
 
     // Respond with a redirect to the authorization URI
     res.status(302); // HTTP 302 Found
